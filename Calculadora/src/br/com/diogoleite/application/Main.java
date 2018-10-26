45e647e9ca6954b153fc1cc8a54284240c1ab29c
Diogo Leite <diogo.development@gmail.com>
2018-10-26 15:21:44 -0300

    Criando toda a interface grafica

--- Calculadora/src/br/com/diogoleite/application/Main.java
+++ Calculadora/src/br/com/diogoleite/application/Main.java
@@ -5,10 +5,28 @@
  */
 package br.com.diogoleite.application;
 
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author diogo_leite
  */
-public class Main{ 
-    
+public class Main extends Application{ 
+    @Override
+    public void start(Stage stage){
+        try{
+            Parent parent = FXMLLoader.load(getClass().getResource("/br/com/diogoleite/view/FXMLCalculadora.fxml"));
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        }catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
 }
--- Calculadora/src/br/com/diogoleite/controller/FXMLCalculadoraController.java
+++ Calculadora/src/br/com/diogoleite/controller/FXMLCalculadoraController.java
@@ -5,10 +5,83 @@
  */
 package br.com.diogoleite.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author diogo_leite
  */
-public class FXMLCalculadoraController {
+public class FXMLCalculadoraController implements Initializable {
+
+    
+    @FXML
+    private TextField txtValor;
+
+    @FXML
+    private Button btnLimpar;
+
+    @FXML
+    private Button btnDesligar;
+
+    @FXML
+    private Button btnSete;
+
+    @FXML
+    private Button btnOito;
+
+    @FXML
+    private Button btnNove;
+
+    @FXML
+    private Button btnQuatro;
+
+    @FXML
+    private Button btnCinco;
+
+    @FXML
+    private Button btnSeis;
+
+    @FXML
+    private Button btnUm;
+
+    @FXML
+    private Button btnDois;
+
+    @FXML
+    private Button btnTres;
+
+    @FXML
+    private Button btnZero;
+
+    @FXML
+    private Button btnZeroDuplo;
+
+    @FXML
+    private Button btnPonto;
+
+    @FXML
+    private Button btnDivisao;
+
+    @FXML
+    private Button btnSubtracao;
+
+    @FXML
+    private Button btnMultiplicacao;
+
+    @FXML
+    private Button btnSoma;
+
+    @FXML
+    private Button btnIgual;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
 
 }
